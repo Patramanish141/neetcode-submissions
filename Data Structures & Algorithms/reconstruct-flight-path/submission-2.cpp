817f3f4ee5370b5a777d5586120aaf396142d37f
Patramanish141 <Patramanish141@gmail.com>
2026-08-14 16:51:18 +0530

    Add: reconstruct-flight-path - submission-2

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<string, multiset<string>> adj;
+    vector<string> result;
+
+    void dfs(string u){
+        while(!adj[u].empty()){
+            string v = *adj[u].begin();
+            adj[u].erase(adj[u].begin());
+            dfs(v);
+        }
+        result.push_back(u);   // add AFTER exhausting all edges from u
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for(auto &t : tickets){
+            adj[t[0]].insert(t[1]);
+        }
+
+        dfs("JFK");
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
